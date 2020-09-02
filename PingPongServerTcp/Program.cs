e9f330d7e41709b37a3b2f7f19275296a4c08b6a
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-02 10:52:58 +0300

    add server tcp

---
+++ PingPongServerTcp/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace PingPongServerTcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+
+            TcpListener serverSocket = new TcpListener(ipAddr, 8888);
+            TcpClient clientSocket = default(TcpClient);
+
+            serverSocket.Start();
+            Console.WriteLine(" >> " + "Server Started, wait to connection...");
+            var counter = 0;
+
+            while (true)
+            {
+                counter += 1;
+                clientSocket = serverSocket.AcceptTcpClient();
+
+                Console.WriteLine(" >> " + "Client No:" + Convert.ToString(counter) + " started!");
+                // handleClinet client = new handleClinet();
+                //client.startClient(clientSocket, Convert.ToString(counter));
+            }
+
+            clientSocket.Close();
+            serverSocket.Stop();
+            Console.WriteLine(" >> " + "exit");
+            Console.ReadLine();
+
+        }
+    }
+    public class HandleClient
+    {
+        TcpClient clientSocket;
+
+        public void StartClient(TcpClient inClientSocket)
+        {
+            this.clientSocket = inClientSocket;
+            Thread ctThread = new Thread(DoChat);
+            ctThread.Start();
+        }
+
+        private void DoChat()
+        {
+            byte[] bytesFrom = new byte[10025];
+            string dataFromClient = null;
+            Byte[] sendBytes = null;
+
+            while ((true))
+            {
+                try
+                {
+                    NetworkStream networkStream = clientSocket.GetStream();
+
+                    networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+                    dataFromClient = Encoding.ASCII.GetString(bytesFrom);
+                  //  dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
+                    Console.WriteLine(" >> " + "From client-" + dataFromClient);
+
+                    sendBytes = Encoding.ASCII.GetBytes(dataFromClient);
+
+                    networkStream.Write(sendBytes, 0, sendBytes.Length);
+                    networkStream.Flush();
+                    Console.WriteLine(" >> " + dataFromClient);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(" >> " + ex.ToString());
+                }
+            }
+        }
+    }
+}
