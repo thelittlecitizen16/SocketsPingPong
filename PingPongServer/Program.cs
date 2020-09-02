ca3cd316bef76aa8327a8f31a062c3080069e748
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-09-02 09:22:06 +0300

    add server

---
+++ PingPongServer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PingPongServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                         SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ... ");
+                    Socket clientSocket = listener.Accept();
+
+                    byte[] bytes = new Byte[1024];
+                    string data = null;
+
+                    int numByte = clientSocket.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes,
+                                               0, numByte);
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    clientSocket.Send(message);
+
+                    
+                   // clientSocket.Shutdown(SocketShutdown.Both);
+                   // clientSocket.Close();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+} 
+
+        }
+    }
+}
